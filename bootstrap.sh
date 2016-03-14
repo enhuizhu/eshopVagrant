495e0ccfb32506c031612faacfa6a825a6e7d0b8
rocabee-david <david@rocabee.com>
2016-03-14 22:39:38 +0000

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get upgrade
+apt-get -f install
+apt-get install -y nginx
+apt-get install -y mysql-server
+apt-get install -y php5-mysql
+apt-get install -y php5-fpm
+apt-get install -y curl php5-cli git
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+#install node.js
+sudo apt-get install -y nodejs
+
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+
+cd /var/www
+#mkdir html
+cd html
+
+git clone https://enhuizhu@bitbucket.org/enhuizhu/eshop.git
